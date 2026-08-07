c57975b7123659610640c9e37ae38f6206ba1291
Mahdi Hosseini <mahdidvb72@gmail.com>
2026-08-07 12:31:01 +0330

    Fix Breadcrumb RealTime Update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import { useData, useRoute, withBase } from 'vitepress'
+import { useData, useRoute } from 'vitepress'
 import { NolebaseBreadcrumbs } from '@nolebase/vitepress-plugin-breadcrumbs/client'
 
 import './style.css'
@@ -12,15 +12,17 @@ import './custom.css'
 export default {
   extends: DefaultTheme,
   Layout: () => {
+    const route = useRoute()
+
     return h(DefaultTheme.Layout, null, {
       'doc-before': () => {
-        const { frontmatter, page, site } = useData()
+        const { frontmatter, page } = useData()
 
         const title = frontmatter.value.title || page.value.title
         if (!title) return null
 
-        return h('div', { class: 'page-header' }, [
-          h(NolebaseBreadcrumbs),
+        return h('div', { class: 'page-header', key: route.path }, [
+          h(NolebaseBreadcrumbs, { key: route.path }),
           h('h1', { id: 'page-title', class: 'page-title' }, title)
         ])
       }
